eb605d4da41540208353955c29c89b69550ba68f
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-13 14:46:44 -0300

    Implementando funções do Hack

---
+++ FunctionsHack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace experimental_hack_ac
+{
+    internal class FunctionsHack
+    {
+    }
+}
