8c6f50c7c8a3c9c4423e86bd4c91a4f86a8213dc
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-13 15:01:10 -0300

    Informações do player Local definidas

--- Player.cs
+++ Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Memory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace experimental_hack_ac
 {
     internal class Player
     {
+        private Mem game = new Mem();
+
+        public string playerobject = "ac_client.exe+0017E0A8";
+        public string name = ",0x205";
+        public string health = ",0xEC";
+        public string shield = ",0xF0";
+        public string bullets = ",0x140";
+        public string pbullets = ",0x12C";
+        public string explosive = ",0x144";
+        public string Y = ",0x2C";
+        public string X = ",0x28";
+        public string Z = ",0x30";
+
+        private string namee = "";
+        private int healthh;
+        private int shieldd;
+        private int bulletss;
+        private int pbulletss;
+        private int explosivee;
+        private float Xx;
+        private float Yy;
+        private float Zz;
+        private int pid;
+
+        public Player()
+        {
+            game.OpenProcess(getPid());
+        }
+        public int getPid()
+        {
+            pid = game.GetProcIdFromName("ac_client.exe");
+            return pid;
+        }
     }
 }
