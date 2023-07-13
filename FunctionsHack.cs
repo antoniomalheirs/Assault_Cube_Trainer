2a8e4ba9f7e44a4f7be5f6b24e41406bc0059827
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-13 15:14:53 -0300

    Funções basicas

--- FunctionsHack.cs
+++ FunctionsHack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Memory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace experimental_hack_ac
 {
     internal class FunctionsHack
     {
+        private Mem game = new Mem();
+        private readonly Player player = new Player();
+
+        private int pid;
+
+        public FunctionsHack()
+        {
+            pid = game.GetProcIdFromName("ac_client.exe");
+            game.OpenProcess(pid);
+        }
     }
 }
